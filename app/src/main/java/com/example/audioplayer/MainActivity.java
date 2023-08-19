46b0e4b927526112f9739b53df8315015879789b
devmarufurrahaman <mdmarufur5682@gmail.com>
2023-08-19 11:16:45 +0600

    music play successfully

--- app/src/main/java/com/example/audioplayer/MainActivity.java
+++ app/src/main/java/com/example/audioplayer/MainActivity.java
@@ -6,15 +6,21 @@ import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
+    Button playBtn, pauseBtn, stopBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        playBtn = findViewById(R.id.play);
+        pauseBtn = findViewById(R.id.pause);
+        stopBtn = findViewById(R.id.stop);
 
         MediaPlayer mp = new MediaPlayer();
         mp.setAudioStreamType(AudioManager.STREAM_MUSIC);
@@ -29,6 +35,36 @@ public class MainActivity extends AppCompatActivity {
             throw new RuntimeException(e);
         }
 
+
+        // play
+        playBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                mp.start();
+            }
+        });
+
+
+        //pause btn
+        pauseBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                mp.pause();
+            }
+        });
+
+
+        // stop btn
+        stopBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                mp.pause();
+                mp.seekTo(0);
+            }
+        });
 
     }
 }
